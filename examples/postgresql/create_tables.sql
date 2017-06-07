d899f52f353375b57704b37042ba47f445319e11
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-06-07 11:43:57 +0900

    PostgreSQL fix

---
+++ examples/postgresql/create_tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY,
+	name TEXT,
+	misc TEXT,
+	created_at TIMESTAMP
+);
--- fetch_tables.go
+++ fetch_tables.go
@@ -2,7 +2,6 @@ package xlsx4db
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	"reflect"
 	"strings"
@@ -112,16 +111,27 @@ func buildUpsertQueryMySQL(db *sql.DB, table string, columns []string) (string, 
 }
 
 func buildUpsertQueryPostgreSQL(db *sql.DB, table string, columns []string) (string, error) {
-	// TODO:
-	return "", errors.New("BuildUpsertQuery for PostgreSQL is not supported yet")
+	placeholders := make([]string, len(columns))
+	updates := make([]string, len(columns))
+	for i, cname := range columns {
+		placeholders[i] = fmt.Sprintf("$%d", i+1)
+		updates[i] = fmt.Sprintf("%s=$%d", cname, i+len(columns)+1)
+	}
+	q := fmt.Sprintf(
+		"INSERT INTO %s (%s) VALUES (%s) ON CONFLICT DO UPDATE SET %s",
+		table,
+		strings.Join(columns, ", "),
+		strings.Join(placeholders, ", "),
+		strings.Join(updates, ", "))
+	return q, nil
 }
 
 func BuildUpsertQuery(db *sql.DB, table string, columns []string) (string, error) {
 	if isMySQL(db) {
 		return buildUpsertQueryMySQL(db, table, columns)
 	}
-	if isPostgreSQL(db) {
-		return buildUpsertQueryPostgreSQL(db, table, columns)
-	}
-	return "", fmt.Errorf("not supported DB: %#v", dbType(db))
+	//if isPostgreSQL(db) {
+	//	return buildUpsertQueryPostgreSQL(db, table, columns)
+	//}
+	return "", fmt.Errorf("BuildUpsertQuery don't supported DB: %#v", dbType(db))
 }
